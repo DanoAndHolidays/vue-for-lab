90ae83d97e8196f7436a93e1636ed66a74f3075c
unknown <3353627694@qq.com>
2025-04-13 15:12:22 +0800

    实现了进度条的数据绑定和颜色调整

--- src/views/Home/components/Hardwave.vue
+++ src/views/Home/components/Hardwave.vue
@@ -1,31 +1,94 @@
 <script lang="ts" setup>
 import {
     ArrowRight,
-    CaretBottom,
     CaretTop,
     Warning,
+    Calendar
 } from '@element-plus/icons-vue'
-import { ref, onMounted } from 'vue'
-import { Minus, Plus } from '@element-plus/icons-vue'
+import { ref, onMounted, } from 'vue'
 import dayjs from 'dayjs'
-import { Calendar } from '@element-plus/icons-vue'
 
+
+//性能消耗
+const cost = ref(76)
+//
 const value = ref(true)
 //设置span
 const span = 6
 //电池电量
-const batteryCharge = ref(100);
+const batteryCharge = ref(95);
+//风扇负压
+const pressure = ref(5600);
+//预估温度
+const temperature = ref(20);
+//设备负载
+const load = ref(45);
+//通信质量
+const communicationQuality = ref(56);
+//设备高度
+const height = ref(10);
+//运行状态
+const status = ref({
+    status1: '良好',
+    status2: '平稳',
+    status3: '差'
+});
+//推荐计划
+const plant = ref({
+    plant1: '继续作业',
+    plant2: '谨慎运行',
+    plant3: '建议返航'
+});
+//电机转速
+const velocity = ref(2000);
+//运行时间
+const runTime = ref(5);
+//当前时间
+const currentTime = ref('');
+const currentData = ref(dayjs().format('YYYY-MM-DD'))
 
+let intervalId;
+
+const updateTime = () => {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    currentTime.value = `${hours}:${minutes}:${seconds}`;
+};
+
+onMounted(() => {
+    // 首次调用以显示时间
+    updateTime();
+    // 每秒更新一次时间
+    intervalId = setInterval(updateTime, 1000);
+});
+
+onMounted(() => {
+    setInterval(() => {
+        percentage2.value = (percentage2.value % 100) + 10
+    }, 500)
+})
 
 const percentage = ref(10)
 const percentage2 = ref(0)
 
-const colors = [
-    { color: '#f56c6c', percentage: 20 },
-    { color: '#e6a23c', percentage: 40 },
-    { color: '#5cb87a', percentage: 60 },
-    { color: '#1989fa', percentage: 80 },
-    { color: '#6f7ad3', percentage: 100 },
+const colors1 = [
+    { color: '#c0392b', percentage: 20 },
+    { color: '#e67e22', percentage: 40 },
+    { color: '#f4d03f', percentage: 60 },
+    { color: '#2ecc71 ', percentage: 80 },
+    { color: '#27ae60', percentage: 100 }
+
+]
+
+const colors2 = [
+
+    { color: '#27ae60', percentage: 20 },
+    { color: '#2ecc71 ', percentage: 40 },
+    { color: '#f4d03f', percentage: 60 },
+    { color: '#e67e22', percentage: 80 },
+    { color: '#c0392b', percentage: 100 }
 ]
 
 const increase = () => {
@@ -40,19 +103,6 @@ const decrease = () => {
         percentage.value = 0
     }
 }
-onMounted(() => {
-    setInterval(() => {
-        percentage2.value = (percentage2.value % 100) + 10
-    }, 500)
-})
-
-
-const value1 = ref(Date.now() + 1000 * 60 * 60 * 24 * 2)
-const value2 = ref(dayjs().add(1, 'month').startOf('month'))
-
-function reset() {
-    value1.value = Date.now() + 1000 * 60 * 60 * 24 * 2
-}
 
 </script>
 
@@ -64,7 +114,7 @@ function reset() {
 
         <el-col span="6">
             <div class="statistic-card">
-                <el-statistic :value="56" title="">
+                <el-statistic :value="communicationQuality" title="">
                     <template #title>
                         <div style="display: inline-flex; align-items: center">
                             通信质量
@@ -79,7 +129,7 @@ function reset() {
                 </el-statistic>
                 <div class="statistic-footer">
                     <div class="footer-item">
-                        <span>than yesterday</span>
+                        <span>于一分钟前</span>
                         <span class="green">
                             16%
                             <el-icon>
@@ -97,7 +147,7 @@ function reset() {
         </el-col>
         <el-col span="6">
             <div class="statistic-card">
-                <el-statistic :value="5600">
+                <el-statistic :value="pressure">
                     <template #title>
                         <div style="display: inline-flex; align-items: center">
                             风扇负压
@@ -127,7 +177,7 @@ function reset() {
             <!--环形进度条-->
             <el-row gutter="10" justify="space-evenly">
                 <el-col>
-                    <el-progress type="dashboard" :percentage="80">
+                    <el-progress type="dashboard" :percentage="batteryCharge" :color="colors1">
                         <template #default="">
                             <span class="percentage-value">{{ batteryCharge }}%</span>
                             <br>
@@ -141,11 +191,11 @@ function reset() {
             <!--环形进度条-->
             <el-row gutter="10" justify="space-evenly">
                 <el-col>
-                    <el-progress type="dashboard" :percentage="80">
+                    <el-progress type="dashboard" :percentage="load" :color="colors2">
                         <template #default="">
-                            <span class="percentage-value">{{ batteryCharge }}%</span>
+                            <span class="percentage-value">{{ load }}%</span>
                             <br>
-                            <span class="percentage-label">电池电量</span>
+                            <span class="percentage-label">设备负载</span>
                         </template>
                     </el-progress>
                 </el-col>
@@ -158,22 +208,22 @@ function reset() {
 
         <el-col span="6">
             <div class="statistic-card">
-                <el-statistic :value="56" title="">
+                <el-statistic :value="temperature" title="">
                     <template #title>
                         <div style="display: inline-flex; align-items: center">
-                            通信质量
-                            <el-tooltip effect="dark" content="当前的通信延迟" placement="top">
+                            预估温度
+                            <el-tooltip effect="dark" content="设备当前核心的预估温度" placement="top">
                                 <el-icon style="margin-left: 4px" :size="12">
                                     <Warning />
                                 </el-icon>
                             </el-tooltip>
                         </div>
                     </template>
-                    <template #suffix>ms</template>
+                    <template #suffix>℃</template>
                 </el-statistic>
                 <div class="statistic-footer">
                     <div class="footer-item">
-                        <span>than yesterday</span>
+                        <span>于一分钟前</span>
                         <span class="green">
                             16%
                             <el-icon>
@@ -191,18 +241,18 @@ function reset() {
         </el-col>
         <el-col span="6">
             <div class="statistic-card">
-                <el-statistic :value="5600">
+                <el-statistic :value="height">
                     <template #title>
                         <div style="display: inline-flex; align-items: center">
-                            风扇负压
-                            <el-tooltip effect="dark" content="代表当前风扇所产生的平均负压" placement="top">
+                            设备高度
+                            <el-tooltip effect="dark" content="当前设备预计的高度" placement="top">
                                 <el-icon style="margin-left: 4px" :size="12">
                                     <Warning />
                                 </el-icon>
                             </el-tooltip>
                         </div>
                     </template>
-                    <template #suffix>Pa</template>
+                    <template #suffix>m</template>
                 </el-statistic>
                 <div class="statistic-footer">
                     <div class="footer-item">
@@ -219,18 +269,18 @@ function reset() {
         </el-col>
         <el-col span="6">
             <div class="statistic-card">
-                <el-statistic :value="56" title="">
+                <el-statistic :value="velocity" title="">
                     <template #title>
                         <div style="display: inline-flex; align-items: center">
-                            通信质量
-                            <el-tooltip effect="dark" content="当前的通信延迟" placement="top">
+                            电机转速
+                            <el-tooltip effect="dark" content="电机当前的转速" placement="top">
                                 <el-icon style="margin-left: 4px" :size="12">
                                     <Warning />
                                 </el-icon>
                             </el-tooltip>
                         </div>
                     </template>
-                    <template #suffix>ms</template>
+                    <template #suffix>rpm</template>
                 </el-statistic>
                 <div class="statistic-footer">
                     <div class="footer-item">
@@ -252,18 +302,18 @@ function reset() {
         </el-col>
         <el-col span="6">
             <div class="statistic-card">
-                <el-statistic :value="5600">
+                <el-statistic :value="runTime">
                     <template #title>
                         <div style="display: inline-flex; align-items: center">
-                            风扇负压
-                            <el-tooltip effect="dark" content="代表当前风扇所产生的平均负压" placement="top">
+                            运行时间
+                            <el-tooltip effect="dark" content="代表当前终端推荐的操作" placement="top">
                                 <el-icon style="margin-left: 4px" :size="12">
                                     <Warning />
                                 </el-icon>
                             </el-tooltip>
                         </div>
                     </template>
-                    <template #suffix>Pa</template>
+                    <template #suffix>min</template>
                 </el-statistic>
                 <div class="statistic-footer">
                     <div class="footer-item">
@@ -283,28 +333,25 @@ function reset() {
     <!-- //第四行的三个信息 -->
     <el-row justify="space-evenly">
         <el-col :span="8">
-            <el-countdown title="电机转速" :value="value1" />
+            <p>运行状态</p>
+            <p>{{ status.status1 }}</p>
         </el-col>
 
         <el-col :span="8">
-            <el-countdown title="运行时间" format="HH:mm:ss" :value="value1" />
-            <!-- <el-button class="countdown-footer" type="primary" @click="reset">
-                Reset
-            </el-button> -->
+            <p>推荐计划</p>
+            <p>{{ plant.plant1 }}</p>
         </el-col>
 
         <el-col :span="8">
-            <el-countdown format="HH:mm:ss" :value="value2">
-                <template #title>
-                    <div style="display: inline-flex; align-items: center">
-                        <el-icon style="margin-right: 4px" :size="12">
-                            <Calendar />
-                        </el-icon>
-                        当前时间
-                    </div>
-                </template>
-            </el-countdown>
-            <div class="countdown-footer">{{ value2.format('YYYY-MM-DD') }}</div>
+
+            <div style="display: inline-flex; align-items: center">
+                <el-icon style="margin-right: 4px" :size="12">
+                    <Calendar />
+                </el-icon>
+                当前时间
+            </div>
+            <P>{{ currentTime }}</P>
+            <div class="countdown-footer">{{ currentData }}</div>
         </el-col>
     </el-row>
     <el-divider />
@@ -312,18 +359,18 @@ function reset() {
     <el-row justify="space-evenly">
         <el-col span="6">
             <!--环形进度条-->
-            <el-progress type="dashboard" :percentage="80">
+            <el-progress type="dashboard" :percentage="cost" :color="colors2">
                 <template #default="">
-                    <span class="percentage-value">{{ batteryCharge }}%</span>
+                    <span class="percentage-value">{{ cost }}%</span>
                     <br>
-                    <span class="percentage-label">电池电量</span>
+                    <span class="percentage-label">性能消耗</span>
                 </template>
             </el-progress>
         </el-col>
         <el-col span="12">
-            <el-slider v-model="value" vertical height="100px" />
-            <el-slider v-model="value" vertical height="100px" />
-            <el-slider v-model="value" vertical height="100px" />
+            <el-slider v-model="batteryCharge" vertical height="100px" />
+            <el-slider v-model="load" vertical height="100px" />
+            <el-slider v-model="cost" vertical height="100px" />
         </el-col>
         <el-col span="6">
 
